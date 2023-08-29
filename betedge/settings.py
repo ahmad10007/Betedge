27af8fd4e5821af0e361a773ed2280622355a624
usama31 <usama@foundersnetwork.com>
2023-08-29 13:32:40 +0500

    Allowed Hosts change

--- betedge/settings.py
+++ betedge/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY", default= "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default = False)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default = []).split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
