890a71ac0ba416a331c5e9c339e3184984e1c815
usama31 <usama@foundersnetwork.com>
2023-08-29 13:42:38 +0500

    static files added

--- betedge/settings.py
+++ betedge/settings.py
@@ -205,7 +205,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+
+# Set the directory where collectstatic will gather static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
